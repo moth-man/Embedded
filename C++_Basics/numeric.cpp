fe68917870de7874e0769707d3148f9a4cd91580
moth-man <mothmanor@gmail.com>
2019-06-12 07:27:27 -0700

    Example of numeric library, creating an inner product vector calculator.

---
+++ C++_Basics/numeric.cpp
@@ -0,0 +1,22 @@
+// Inner product example using numeric library.
+// u * v = uv
+#include <array>
+#include <numeric>
+
+const std::array<int, 3U> u {
+
+  { 1, 2, 3 }
+
+};
+
+const std::array<int, 3U> v {
+
+  { 4, 5, 6 }
+
+};
+
+const in uv = std::inner_product(u.begin(),
+                                 u.end(),
+                                 v.begin(),
+                                 0);
+// Result is 32.
