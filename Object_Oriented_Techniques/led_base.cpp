671b1d52f3dbcc93a2be814dee7593420d27280e
moth-man <mothmanor@gmail.com>
2019-06-12 07:47:17 -0700

    Create base class for object oriented led class structure.

---
+++ Object_Oriented_Techniques/led_base.cpp
@@ -0,0 +1,22 @@
+class led_base {
+
+  public:
+    virtual boid toggle() = 0; // Pure abstract.
+    virtual ~led_base() { }    // Virtual destructor.
+
+    // Interface for querying the LED state.
+    bool state_is_on() const { return is_on; }
+
+  protected:
+    bool is_on;
+
+    // A protected default constructor.
+    led_base() : is_on(false) { }
+  
+  private:
+    // Private non-implemented copy constructor.
+    led_base(const led_base&) = delete;
+
+    // Private non-implemented copy assignment operator.
+    const led_base& operator=(const led_base&) = delete;
+};
